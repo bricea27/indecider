add6d76a0797340de1ebd2b793dd351a73b46e96
bricea27 <bricea27@gmail.com>
2016-03-06 12:16:51 -0500

    updated everything

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,89 +1,87 @@
-// document.addEventListener("DOMContentLoaded", function() {
+// VARIABLES
+var body = document.body;
+var questionContainer = document.getElementById("questionContainer");
+var question = document.getElementById("question");
+var submit = document.getElementById("submit");
+var answer = document.getElementById("answer");
+var answerContainer = document.getElementById("answerContainer");
+var newQuestion = document.getElementById("newQuestion");
+var loader = document.getElementById("loader");
 
-// 	var question = document.getElementById('question');
-// 	var submit = document.getElementById('submit');
-// 	var newQuestion = document.getElementById('newQuestion');
-// 	var answer = document.getElementById('answer');
+var colors = ['turqoise', 'emerald', 'peterRiver', 'amethyst', 'wetAsphalt', 'carrot', 'alizarin'];
+var yesAnswers = ['Duh!', 'Heck Yes', 'One Hundred Percent', 'Uh, Yeah', 'Oh ya for definite'];
+var noAnswers = ['Not a Chance', 'No Way José', "You're Joking, Right?", "Uh, No", "Nope Nope Nope", "Negative, Chief"];
+var loaderMessages = ["I'm thinking...", "Give me a sec there sport...", "Processing request...", "You would ask that..."];
 
-// 	var questionSection = document.getElementById('questionSection');
-// 	var answerSection = document.getElementById('answerSection');
+submit.addEventListener("click", function(){
+	var questionText = question.value.trim();
+	if (questionText.length > 0) {
+		answerQuestion(questionContainer, answer, answerContainer);
+	}
+})
 
-// 	submit.addEventListener('click', function(){
-		
-// 		var url = 'http://yesno.wtf/api/';
-// 		var xhr = new XMLHttpRequest();
-// 		xhr.open('GET', url);
-// 		xhr.addEventListener('load', function(e){
-// 			var data = xhr.responseText;
-// 			var parsed = JSON.parse(data);
-// 			questionSection.style.display="none";
-// 			question.value = "";
-// 			answerSection.style.display="block";
-// 			answer.innerHTML = parsed.answer;
-// 			answerSection.style.backgroundImage = "url('" + parsed.image + "')";
-// 		});
-// 		xhr.send();
-// 	});
-
-// 	newQuestion.addEventListener('click', function() {
-// 		answerSection.style.display="none";
-// 		questionSection.style.display="block";
-// 		question.focus();
-// 	});
-
-
-
-// }); //end document ready
-
-$(document).ready(function(){
-
-	var question = $('#question');
-	var submit = $('#submit');
-	var newQuestion = $('#newQuestion');
-	var answer = $('#answer');
-	var description = $('#description');
-
-	var questionSection = $('#questionSection');
-	var answerSection = $('#answerSection');
-
-	submit.click(function(){
-		answerSection.css('backgroundImage', 'none');
-		questionSection.fadeOut('slow');
-		question.fadeOut('slow');
-		submit.fadeOut('slow');
-		answerSection.fadeIn('slow');
-		answer.fadeIn('slow');
-		newQuestion.fadeIn('slow');
-		getAnswer(answer, answerSection);
-	});
-
-	newQuestion.click(function(){
-		answerSection.fadeOut('slow');
-		answer.fadeOut('slow');
-		newQuestion.fadeOut('slow');
-		questionSection.fadeIn('slow');
-		question.val('');
-		question.fadeIn('slow')
-		submit.fadeIn('slow');
-		question.focus();
-	});
-
-	function getAnswer(elem1, elem2){
-		$.ajax({
-			url: 'http://yesno.wtf/api/', 
-			dataType: 'json',
-			success: function(data) {
-					elem1.html(data.answer + '!');
-
-					// if (data.answer === "no") {
-					// 	elem3.html("You shouldn't " + question);
-					// } else {
-					// 	elem3.html("You should " + question);
-					// }
-
-					elem2.css("backgroundImage", "url('" + data.image + "')");
-				}
-		});
-	};
-
+question.addEventListener("keydown", function(e) {
+	var questionText = question.value.trim();
+	if (e.keyCode == 13 && questionText.length > 0) {
+		answerQuestion(questionContainer, answer, answerContainer);
+	}
 });
+
+newQuestion.addEventListener("click", function(){
+	askAgain();
+});
+
+// on document ready
+document.addEventListener("DOMContentLoaded", function() {
+	randomBackground(colors, questionContainer);
+});
+
+// FUNCTIONS
+function randomBackground(colors, element) {
+	element.className = colors[Math.round(Math.random() * (colors.length - 1))];
+}
+
+function randomAnswer(array, element) {
+	var randomAnswer = array[Math.round(Math.random() * (array.length - 1))];
+	element.innerHTML = randomAnswer;
+}
+
+function answerQuestion(questionContainer, answer, answerContainer) {
+	randomLoader(loaderMessages, loader);
+	questionContainer.className = "fadeOut";
+	loader.style.display="";
+	setTimeout(function(){
+		loader.style.display="none";
+		answerContainer.style.display="";
+	}, 2000);
+	var url = "http://yesno.wtf/api/";
+	var request = new XMLHttpRequest();
+	request.open("GET", url, true);
+	request.onload = function() {
+		if (request.status >= 200 && request.status < 400) {
+			var response = JSON.parse(request.responseText);
+			if (response['answer'] === "yes") {
+				randomAnswer(yesAnswers, answer);
+			} else {
+				randomAnswer(noAnswers, answer);
+			};
+			questionContainer.style.display="none";
+			question.value = "";
+			answerContainer.style.background = "url('" + response['image'] + "') no-repeat center center";
+		}
+	}
+	request.send();
+}
+
+function askAgain() {
+	questionContainer.style.display="";
+	question.focus();
+	randomBackground(colors, questionContainer);
+	answerContainer.style.display = "none";
+	loader.style.display="none";
+}
+
+function randomLoader(array, element) {
+	var randomMessage = array[Math.round(Math.random() * (array.length - 1))];
+	element.innerHTML = randomMessage;
+}
